e2d4e17f6a6b7305c991e01c5e364697cc3a8968
elorenzen <eric.lorenzen@gmail.com>
2019-06-02 12:18:53 -0600

    Added componentDidMount handler that loads Google API email data with Oauth from client_ID

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
+const client_id = process.env.CLIENT_ID;
+
 class GoogleAuth extends React.Component {
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: client_id,
+                scopes: 'email'
+            })
+        });
+    }
+
     render() {
         return <div>Google Auth</div>
     };
