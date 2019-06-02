f8b232e0c0e403720ade4fab8b8ff5766f2f97b0
elorenzen <eric.lorenzen@gmail.com>
2019-06-02 09:45:51 -0600

    Created basic components for desired pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const Landing = () =>{
+const streamList = () =>{
     return(
         <div>
-            Landing Page
-            <Link to="/pagetwo">See page two</Link>
+            Stream List(landing)
+            <Link to="/streams/show">Show stream page</Link>
+            <Link to='/streams/new'>New stream page</Link>
+            <Link to='/streams/edit'>Edit stream page</Link>
+            <Link to='/streams/delete'>Delete stream page</Link>
         </div>
     )
 }
 
-const PageTwo = () => {
+const showStream = () => {
     return (
         <div>
-            Page two
+            Stream Show
+            <Link to='/'>Back home</Link>
+        </div>
+    )
+}
+
+const newStream = () => {
+    return (
+        <div>
+            Create new stream
+            <Link to='/'>Back home</Link>
+        </div>
+    )
+}
+
+const editStream = () => {
+    return (
+        <div>
+            edit stream
+            <Link to='/'>Back home</Link>
+        </div>
+    )
+}
+
+const deleteStream = () => {
+    return (
+        <div>
+            delete stream
             <Link to='/'>Back home</Link>
         </div>
     )
@@ -23,8 +53,11 @@ const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <Route path="/" exact component={Landing} />
-                <Route path='/pagetwo' component={PageTwo} />
+                <Route path="/" exact component={streamList} />
+                <Route path='/streams/show' component={showStream} />
+                <Route path='/streams/new' component={newStream} />
+                <Route path='/streams/edit' component={editStream} />
+                <Route path='/streams/delete' component={deleteStream} />
             </BrowserRouter>
         </div>
     )
