55c8cab8daa930d2f95b3b600a90af8dc926a680
elorenzen <eric.lorenzen@gmail.com>
2019-06-02 10:29:30 -0600

    Imported and added Header component into App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,19 +6,23 @@ import EditStream from '../components/streams/EditStream';
 import NewStream from '../components/streams/NewStream';
 import ShowStream from '../components/streams/ShowStream';
 import StreamList from '../components/streams/StreamList';
+import Header from '../components/Header';
 
 const App = () => {
     return (
         <div>
+            <Header />
             <BrowserRouter>
+            <div>
                 <Route path="/" exact component={StreamList} />
                 <Route path='/streams/show' component={ShowStream} />
                 <Route path='/streams/new' component={NewStream} />
                 <Route path='/streams/edit' component={EditStream} />
                 <Route path='/streams/delete' component={DeleteStream} />
+            </div>
             </BrowserRouter>
         </div>
-    )
-}
+    );
+};
 
 export default App;
