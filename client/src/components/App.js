b67577727ce6d96fd0449507226f843188752b79
elorenzen <eric.lorenzen@gmail.com>
2019-06-02 09:57:23 -0600

    Removed imported { Link } component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,53 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-const streamList = () =>{
-    return(
-        <div>
-            Stream List(landing)
-            <Link to="/streams/show">Show stream page</Link>
-            <Link to='/streams/new'>New stream page</Link>
-            <Link to='/streams/edit'>Edit stream page</Link>
-            <Link to='/streams/delete'>Delete stream page</Link>
-        </div>
-    )
-}
-
-const showStream = () => {
-    return (
-        <div>
-            Stream Show
-            <Link to='/'>Back home</Link>
-        </div>
-    )
-}
-
-const newStream = () => {
-    return (
-        <div>
-            Create new stream
-            <Link to='/'>Back home</Link>
-        </div>
-    )
-}
-
-const editStream = () => {
-    return (
-        <div>
-            edit stream
-            <Link to='/'>Back home</Link>
-        </div>
-    )
-}
-
-const deleteStream = () => {
-    return (
-        <div>
-            delete stream
-            <Link to='/'>Back home</Link>
-        </div>
-    )
-}
 
 const App = () => {
     return (
