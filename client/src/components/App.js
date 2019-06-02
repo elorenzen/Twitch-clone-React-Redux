b2846ef41cdab8c11e9d974fc0386e0cd1969875
elorenzen <eric.lorenzen@gmail.com>
2019-06-02 09:24:03 -0600

    Imported components from react-router-dom and created two basic components with paths and links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const Landing = () =>{
+    return(
+        <div>
+            Landing Page
+            <Link to="/pagetwo">See page two</Link>
+        </div>
+    )
+}
+
+const PageTwo = () => {
+    return (
+        <div>
+            Page two
+            <Link to='/'>Back home</Link>
+        </div>
+    )
+}
 
 const App = () => {
-    return <div>This is the app component!</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <Route path="/" exact component={Landing} />
+                <Route path='/pagetwo' component={PageTwo} />
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App;
