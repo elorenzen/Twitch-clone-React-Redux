52db244e34dd203c0a871e28fc9c53fb12b68628
elorenzen <eric.lorenzen@gmail.com>
2019-06-02 10:03:05 -0600

    Imported stream component files and amended to value of Route component attribute to match imported component names

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import DeleteStream from '../components/streams/DeleteStream';
+import EditStream from '../components/streams/EditStream';
+import NewStream from '../components/streams/NewStream';
+import ShowStream from '../components/streams/ShowStream';
+import StreamList from '../components/streams/StreamList';
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <Route path="/" exact component={streamList} />
-                <Route path='/streams/show' component={showStream} />
-                <Route path='/streams/new' component={newStream} />
-                <Route path='/streams/edit' component={editStream} />
-                <Route path='/streams/delete' component={deleteStream} />
+                <Route path="/" exact component={StreamList} />
+                <Route path='/streams/show' component={ShowStream} />
+                <Route path='/streams/new' component={NewStream} />
+                <Route path='/streams/edit' component={EditStream} />
+                <Route path='/streams/delete' component={DeleteStream} />
             </BrowserRouter>
         </div>
     )
